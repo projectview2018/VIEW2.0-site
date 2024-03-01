647a3096bbfcf2eb7dc1f556fd0565b46e3d8eef
gabbyblake <gaby.blake2@gmail.com>
2024-03-01 01:02:42 -0500

    testing deployment string error

--- view/settings.py
+++ view/settings.py
@@ -158,7 +158,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
-AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
+# AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
+AWS_DEFAULT_ACL = None
 # Make sure nyc3 is correct
 # AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')

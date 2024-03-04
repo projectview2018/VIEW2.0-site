053845930cb6bb023c7751c5941eaa0f70a3234b
gabbyblake <gaby.blake2@gmail.com>
2024-03-04 17:24:02 -0500

    clarified variables

--- view/settings.py
+++ view/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from boto3 import *
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -156,11 +157,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'lidar/static/')
 
 print(f"Bucket type is {type(os.environ.get('AWS_STORAGE_BUCKET_NAME'))}")
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 # AWS_
-AWS_STORAGE_BUCKET_NAME = "vehicle-scans"
-AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME')
+AWS_STORAGE_BUCKET_NAME = 'vehicle-scans'
+AWS_S3_REGION_NAME = os.environ.get('AWS_REGION_NAME')
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
 # Make sure nyc3 is correct
 # AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'

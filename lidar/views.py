72df9de1bb19e413d79c3386a6e4ee7a6e6858a8
gabbyblake <gaby.blake2@gmail.com>
2024-03-15 15:27:56 -0400

    updated form and models for A,B,C inputs

--- lidar/views.py
+++ lidar/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 import threading
 from .forms import ScanForm, VehicleForm
 from .perform_scan import complete_scan
@@ -20,15 +21,47 @@ def add_vehicle(request):
 
 def data_upload(request):
     if request.method == 'POST':
+        print("request was a POST")
         vehicle_form = VehicleForm(request.POST, request.FILES)
         scan_form = ScanForm(request.POST, request.FILES)
-        if scan_form.is_valid():
-            scan = scan_form.save()
-            vehicle = Vehicle.objects.get(pk=scan.vehicle.id)
+        print(f'scan form errors: {scan_form.errors}')
+        print(f'vehicle form errors: {vehicle_form.errors}')
+        if scan_form.is_valid() and vehicle_form.is_valid():
+            print("Both forms valid")
+            vehicle = vehicle_form.save(commit=False)
+            print("vehicle form saved to a Vehicle object")
+            obj, created = Vehicle.objects.update_or_create(
+                vehicle_make=vehicle.vehicle_make,
+                vehicle_model=vehicle.vehicle_model,
+                vehicle_year=vehicle.vehicle_year,
+                defaults={"vehicle_updated": timezone.now()},
+                create_defaults={"vehicle_make": vehicle.vehicle_make,
+                                 "vehicle_model": vehicle.vehicle_model,
+                                 "vehicle_year": vehicle.vehicle_year,
+                                 "vehicle_body_class": vehicle.vehicle_body_class,
+                                 "vehicle_weight_class": vehicle.vehicle_weight_class}
+            )
+            print("vehicle query successful")
+            scan = scan_form.save(commit=False)
+            if created:
+                print("vehicle not found: assigning new vehicle to scan")
+                scan.vehicle = obj
+                print(
+                    f"new vehicle, {obj.vehicle_make} {obj.vehicle_model} {obj.vehicle_year}, saved")
+            else:
+                print("vehicle found: assigning old vehicle to scan")
+                vehicle = Vehicle.objects.get(
+                    vehicle_make=vehicle.vehicle_make, vehicle_model=vehicle.vehicle_model, vehicle_year=vehicle.vehicle_year)
+                scan.vehicle = vehicle
+                print(
+                    f"previous vehicle, {vehicle.vehicle_make} {vehicle.vehicle_model} {vehicle.vehicle_year}, updated")
+            scan.save()
+            print("scan form saved")
             thread = threading.Thread(
                 target=complete_scan, args=(scan, vehicle))
             thread.start()
-            return HttpResponseRedirect('/add_vehicle')
+            # return HttpResponseRedirect('/windshield_removal')
+            return render(request, 'lidar/windshield-removal.html', {})
     else:
         vehicle_form = VehicleForm()
         scan_form = ScanForm()

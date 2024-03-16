eb372ec6fe7f155a5280387ca3690b2fc4245049
gabbyblake <gaby.blake2@gmail.com>
2024-03-16 12:20:04 -0400

    fixed s3 client connection

--- lidar/s3_utils.py
+++ lidar/s3_utils.py
@@ -3,7 +3,7 @@ import os
 
 
 def create_s3_client():
-    return boto3.client('s3', aws_access_key_id=os.environ.get(
+    return boto3.client('s3', region_name='nyc3', endpoint_url=os.environ.get('AWS_S3_ENDPOINT_URL'), aws_access_key_id=os.environ.get(
         'AWS_S3_ACCESS_KEY_ID'), aws_secret_access_key=os.environ.get('AWS_S3_SECRET_ACCESS_KEY'))
 
 
@@ -14,7 +14,8 @@ def get_object(filepath: str):
     print(f'{bucket = }')
     print(os.environ.get('AWS_S3_ACCESS_KEY_ID'))
     try:
-        response = client.get_object(Bucket=bucket, Key=f'media/lidar/lidar_scans/2011HondaOdysseyScan1.glb')
+        response = client.get_object(
+            Bucket=bucket, Key=f'media/lidar/lidar_scans/2011HondaOdysseyScan1.glb')
     except Exception as e:
         print(e)
         raise e

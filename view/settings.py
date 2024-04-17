325dc97e77cf3dee1bc8664bc53f64e74bf6c505
Alex Matsoukas <amatsoukas@olin.edu>
2024-04-17 14:08:41 -0400

    add csrf trusted origin to settings

--- view/settings.py
+++ view/settings.py
@@ -51,7 +51,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'view2-vd8vm.ondigitalocean.app',
                  'localhost', '127.0.0.1', '0.0.0.0', "view2.blindzonesafety.org"]
-CSRF_TRUSTED_ORIGINS = ['https://view2-vd8vm.ondigitalocean.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://view2-vd8vm.ondigitalocean.app', 'https://view2.blindzonesafety.org']
 
 # Application definition
 

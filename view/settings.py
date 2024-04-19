d20762e652f3f7cbf2375841d0674bb95924e49b
benmor20 <benmor2020@gmail.com>
2024-04-18 22:50:45 -0400

    Turn off debug mode

--- view/settings.py
+++ view/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-kfb=+rej@*g)fb58b_&!o8vg4&!gn&6o6-(749bqdd@b+up9_0
 # # SECURITY WARNING: don't run with debug turned on in production!
 # if not IS_HEROKU_APP:
 #     DEBUG = True
-DEBUG = True
+DEBUG = False
 
 # # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # # validation of the Host header in the incoming HTTP request. On other platforms you may need

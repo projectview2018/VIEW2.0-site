e1814f4abe4975db5d6162d83b6aae157df9e072
Alex Matsoukas <amatsoukas@olin.edu>
2024-05-13 20:55:01 -0400

    finish commenting vehicle db table js file

--- lidar/static/lidar/vehicle-database-table.js
+++ lidar/static/lidar/vehicle-database-table.js
@@ -342,60 +342,114 @@ class Model {
 }
 
 class SearchController {
+  /**
+   * Create a SearchController instance.
+   *
+   * Responsible for updating the current list of vehicle objects
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // associated model
     this.model = m;
+    // get the search bar components from the DOM
+    // field dropdown, search text field, clear button
     this.seachColSelector = document.getElementById("search_col_select");
     this.searchBar = document.getElementById("search_bar");
     this.clearButton = document.getElementById("clear_search_bar");
+    // set the default field to search from
     this.seachColSelector.value = "2";
+    // event handler for search field change
     this.seachColSelector.addEventListener("change", () =>
       this.handleSeachFieldChange()
     );
+    // event handler for text input
     this.searchBar.addEventListener("input", (e) => this.handleSearchInput(e));
+    // event handler for clear button
     this.clearButton.addEventListener("click", () => this.handleClearSearch());
   }
 
+  /**
+   * On search field change, change search field index and update current list.
+   */
   handleSeachFieldChange() {
+    // set index of field to search from
     this.model.search_field_index = +this.seachColSelector.value;
+    // update placeholder text for empty search bar
     this.searchBar.placeholder = `Search for ${
       this.seachColSelector.options[this.model.search_field_index].text
     }`;
+    // if text in searchbar, update current list
     if (this.model.seach_value !== "") {
       this.model.updateCurrentList();
     }
   }
 
+  /**
+   * Update current list of vehicles based on the value in the search field.
+   *
+   * @param {object} e - input event
+   */
   handleSearchInput(e) {
+    // set search value
     this.model.seach_value = e.target.value;
+    // update current list
     this.model.updateCurrentList();
   }
 
+  /**
+   * Update search bar value and search text on clear button click.
+   */
   handleClearSearch() {
+    // set search bar value and search value to empty string
     this.searchBar.value = "";
     this.model.seach_value = "";
+    // update current list
     this.model.updateCurrentList();
   }
 }
 
 class NumRowController {
+  /**
+   * Create a NumRowController instance
+   *
+   * Responsible for setting the number of rows in the table.
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // associated model
     this.model = m;
+    // num row dropdown
     this.numSelect = document.getElementById("num_row_select");
+    // event listener for changed number of rows
     this.numSelect.addEventListener("change", () => this.handleChangedRowNum());
+    // callback function when page changes
     m.subChangedPage(() => this.handleChangedPage());
   }
 
+  /**
+   * Set the number of cars per page based on dropdown value.
+   */
   handleChangedRowNum() {
     let new_num_cars = this.numSelect.value;
+    // if value is "All", use the length of the current list
+    // else, use the value of the dropdown
     if (new_num_cars === "All") {
       this.model.cars_per_page = this.model.current_list.length;
     } else {
       this.model.cars_per_page = +this.numSelect.value;
     }
+    // update the number of pages based on cars per page
     this.model.updateNumPages();
   }
 
+  /**
+   * On page change, set value of dropdown to match cars per page.
+   */
   handleChangedPage() {
+    // if cars per page is the length of the current list, set value to "All"
+    // else, use the value of cars per page
     if (this.model.current_list.length === this.model.cars_per_page) {
       this.numSelect.value = "All";
     } else {
@@ -405,24 +459,46 @@ class NumRowController {
 }
 
 class SortController {
+  /**
+   * Create a SortController instance.
+   *
+   * Responsible for setting sort parameters based on sort dropdown.
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // associated model
     this.model = m;
+    // sort selector dropdown from DOM
     this.sortSelect = document.getElementById("sort_select");
+    // event listener for change sort option
     this.sortSelect.addEventListener("change", () => this.handleChangedSort());
+    // callback function when the page changes
     m.subChangedPage(() => this.handleChangedPage());
   }
 
+  /**
+   * Choose index of sorted list and reverse based on dropdown value.
+   */
   handleChangedSort() {
+    // index of vehicle list is the same for each asc./desc. pair
     this.model.vehicle_lists_index = Math.ceil(this.sortSelect.value / 2);
+    // set reverse based on parity of dropdown value (every other is reversed)
     if (this.sortSelect.value % 2 === 0 && this.sortSelect.value != 0) {
       this.model.reverse = true;
     } else {
       this.model.reverse = false;
     }
+    // update current list
     this.model.updateCurrentList();
   }
 
+  /**
+   * On page change, set the sort select value based on the vehicle list index.
+   */
   handleChangedPage() {
+    // if vehicle list index is 0, set the sort select value to 0
+    // else, multiple by two and subtract 1 if not reversed
     this.sortSelect.value =
       this.model.vehicle_lists_index === 0
         ? this.model.vehicle_lists_index
@@ -431,22 +507,43 @@ class SortController {
 }
 
 class TableView {
+  /**
+   * Create a TableView instance.
+   *
+   * Responsible for populating the rows of the table from the vehicle objects.
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // the associated model
     this.model = m;
+    // the table element from the DOM
     this.table = document.getElementById("vehicle_table");
+    // callback function on page change
     m.subChangedPage(() => this.handleChangedPage());
   }
 
+  /**
+   * On page change, clear the current table and populate the updated rows.
+   */
   handleChangedPage() {
+    // remove all of the old rows of the table
     while (this.table.children.length > 1) {
       this.table.removeChild(this.table.children[1]);
     }
+    // iterate through each of the vehicles to be shown
     for (let vehicle of this.model.getShownVehicles()) {
+      // create table row element and append to table
       let row = document.createElement("tr");
       row.classList.add("elevation01");
       this.table.appendChild(row);
+      // iterate through the fields that go in the table
       for (let index in this.model.table_fields) {
+        // create a table cell element
         let col = document.createElement("td");
+        // if the field is body or weight class,
+        // look up the correct text in the tables defined above
+        // else, use the text in the vehicle object's field
         if (this.model.table_fields[index] === "vehicle_body_class") {
           col.innerText =
             body_class_dict[vehicle.fields[this.model.table_fields[index]]];
@@ -456,135 +553,219 @@ class TableView {
         } else {
           col.innerText = vehicle.fields[this.model.table_fields[index]];
         }
+        // add special class to leftmost (first) field
         if (+index === 0) {
           col.classList.add("left_cell");
         }
+        // append cell to row
         row.appendChild(col);
       }
+      // create an extra cell for the view button
       let id = document.createElement("td");
+      // add special class to rightmost (last) field
       id.classList.add("right_cell");
+      // create dropdown
       let dropdown = createDropdown(vehicle.pk);
+      // append cell to row
       row.appendChild(id);
+      // append dropdown to cell
       id.appendChild(dropdown);
     }
   }
 }
 
 class ViewButtonController {
+  /**
+   * Create a ViewButtonController instance.
+   *
+   * Responsible for updating link on view button based on dropdown value.
+   *
+   * @param {HTMLSelectElement} d - dropdown select element
+   * @param {HTMLAnchorElement} b - link element (view button)
+   */
   constructor(d, b) {
+    // associated dropdown
     this.dropdown = d;
+    // associated button
     this.button = b;
+    // the value of the dropdown corresponding to a scan number
     this.scan_num = this.dropdown.value;
+    // create the link to the vis page based on the scan number
     this.button.href = vis_page_url + "/" + this.scan_num;
 
+    // event listener for dropdown change
     this.dropdown.addEventListener("change", () => this.handleChangedView());
   }
 
+  /**
+   * On scan dropdown change, edit the button url.
+   */
   handleChangedView() {
+    // set the new scan number
     this.scan_num = this.dropdown.value;
+    // set the new url
     this.button.href = vis_page_url + "/" + this.scan_num;
   }
 }
 
 class LabelView {
+  /**
+   *  Create a LabelView instance.
+   *
+   * Responsible for setting the text at the bottom left of the table.
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // associated model
     this.model = m;
+    // label element from the DOM
     this.label = document.getElementById("entry_indicator");
+    // callback function for changed page
     m.subChangedPage(() => this.handleChangedPage());
   }
 
+  /**
+   * On page change, set the start/end vehicle text at the bottom of the table.
+   */
   handleChangedPage() {
     this.label.innerText = `Showing ${this.model.start_vehicle_num}-${this.model.end_vehicle_num} of ${this.model.current_list.length} entries`;
   }
 }
 
 class PageSelectorViewController {
+  /**
+   * Create a PageSelectorViewController instance
+   *
+   * Responsible for creating the page buttons at the bottom right of the table
+   *
+   * @param {object} m - an instance of the Model class
+   */
   constructor(m) {
+    // accociated model
     this.model = m;
+    // page selector element from DOM
     this.pageSelector = document.getElementById("pagination");
+    // callback function for page change
     m.subChangedPage(() => this.handleChangedPage());
   }
 
+  /**
+   * On page change, update the pagintation buttons at the bottom of the table.
+   */
   handleChangedPage() {
+    // remove the old pagination buttons
     while (this.pageSelector.children.length > 0) {
       this.pageSelector.removeChild(this.pageSelector.children[0]);
     }
 
+    // set the start page - should be two behind the current page,
+    // unless current page is 1 or 2
     let start_page = this.model.page - 2 > 0 ? this.model.page - 2 : 1;
+    // set the end page to be 4 more than start page (or max of num_pages)
     let end_page =
       start_page + 4 <= this.model.num_pages
         ? start_page + 4
         : this.model.num_pages;
 
+    // create link element to jump to first page and add classes
     let first_page = document.createElement("a");
     first_page.innerText = "<< First";
     first_page.dataset.pagenum = 1;
     first_page.classList.add("pagination_elt", "page_turn");
+    // button should be clickable if not alread on first page
+    // add event listener for click
     if (this.model.page !== 1) {
       first_page.addEventListener("click", (e) => this.handlePageChange(e));
       first_page.classList.add("clickable_page");
     } else {
       first_page.classList.add("unclickable_page");
     }
+    // append button to page selector
     this.pageSelector.appendChild(first_page);
 
+    // create link element to jump back one page and add classes
     let prev_page = document.createElement("a");
     prev_page.innerText = "< Previous";
     prev_page.dataset.pagenum = this.model.page - 1;
     prev_page.classList.add("pagination_elt", "page_turn");
+    // should be clickable if current page is not 1
+    // add event listener for click
     if (this.model.page !== 1) {
       prev_page.addEventListener("click", (e) => this.handlePageChange(e));
       prev_page.classList.add("clickable_page");
     } else {
       prev_page.classList.add("unclickable_page");
     }
+    // append button to page selector
     this.pageSelector.appendChild(prev_page);
 
+    // loop through the page numbers
     for (let i = start_page; i <= end_page; i++) {
+      // create link element and add classes
       let curr_page = document.createElement("a");
       curr_page.innerText = `${i}`;
       curr_page.dataset.pagenum = i;
       curr_page.classList.add("pagination_elt");
+      // should be clickable if not current page
+      // add event listener for click
+      // else, add special styling for current page
       if (i !== this.model.page) {
         curr_page.addEventListener("click", (e) => this.handlePageChange(e));
         curr_page.classList.add("clickable_page");
       } else {
         curr_page.classList.add("current_page");
       }
+      // append to page selector
       this.pageSelector.appendChild(curr_page);
     }
 
+    // create link element to jump forward one page and add classes
     let next_page = document.createElement("a");
     next_page.innerText = "Next >";
     next_page.dataset.pagenum = this.model.page + 1;
     next_page.classList.add("pagination_elt", "page_turn");
+    // should be clickable if not on last page
+    // add event listener for click
     if (this.model.page !== this.model.num_pages) {
       next_page.addEventListener("click", (e) => this.handlePageChange(e));
       next_page.classList.add("clickable_page");
     } else {
       next_page.classList.add("unclickable_page");
     }
+    // append to page selector
     this.pageSelector.appendChild(next_page);
 
+    // create link to jump to last page and add classes
     let last_page = document.createElement("a");
     last_page.innerText = "Last >>";
     last_page.dataset.pagenum = this.model.num_pages;
     last_page.classList.add("pagination_elt", "page_turn");
+    // should be clickable if not on last page
+    // add event listener for click
     if (this.model.page !== this.model.num_pages) {
       last_page.addEventListener("click", (e) => this.handlePageChange(e));
       last_page.classList.add("clickable_page");
     } else {
       last_page.classList.add("unclickable_page");
     }
+    // append to page selector
     this.pageSelector.appendChild(last_page);
   }
 
+  /**
+   * Callback function to change the page to the clicked vutton value
+   *
+   * @param {object} e - click event
+   */
   handlePageChange(e) {
     this.model.changePage(+e.target.dataset.pagenum);
   }
 }
 
-// initialize model, controllers, and views
+/**
+ * Initialize model, controllers, and views
+ */
 function init() {
   const model = new Model();
   new SearchController(model);

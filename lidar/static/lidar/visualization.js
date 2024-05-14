02696d15f60fc761c1e57d32e7bbf328a3da37ca
Alex Matsoukas <amatsoukas@olin.edu>
2024-05-14 14:46:45 -0400

    finish commenting vis js file

--- lidar/static/lidar/visualization.js
+++ lidar/static/lidar/visualization.js
@@ -76,6 +76,8 @@ if (loadingStatus === true) {
     let graphContainer = document.getElementById("graph_container");
     graphContainer.style.display = "flex";
 
+    // set the text for the header of the graph section to match the vru(s)
+    // use the number of the vru as a lookup into vruLabels
     let graphContainerHeading = document.getElementById("vissVRUshown");
     graphContainerHeading.innerText =
       vrusSelected.length > 1
@@ -84,43 +86,70 @@ if (loadingStatus === true) {
           }`
         : `${vruLabels[vrusSelected[0] - 1]}`;
 
+    // set the text for each of the vru1 labels
     let vru1_fields = document.getElementsByClassName("vru1_label");
     for (let field of vru1_fields) {
       field.innerText = `${vruLabels[vrusSelected[0] - 1]}`;
     }
+
+    // set the value for the min forward distance that vru1 is visible from
     let vru1_front = document.getElementById("vru1_front_dist");
     vru1_front.innerText = `${closest_forward_nvps[0]} ft`;
+
+    // set the total number of vru1s hidden in blindzone
     let vru1_total_num = document.getElementById("vru1_total_num");
     vru1_total_num.innerText = `${num_vrus_in_vru_area[0]}`;
+
+    // set the text for the plural vru1 labels
     let vru1_label_plural = document.getElementById("vru1_label_plural");
     vru1_label_plural.innerText = `${vruLabelsPlural[vrusSelected[0] - 1]}`;
+
+    // make the text for vru1 visible
     let vru1String = document.getElementById("graph_str1");
     vru1String.style.display = "inline";
 
+    // set the text in the legend for vru1
     let vru1Text = document.getElementById("vru1_entry_text");
     vru1Text.innerText = `${vruLabels[vrusSelected[0] - 1]} blindzone`;
 
+    // set the color of the legend to match the graph color
     let vru1Color = document.getElementById("vru1_entry_color");
     vru1Color.style.backgroundColor = colors[0];
     vru1Color.style.border = borders[0];
 
+    // repeat process if there is second vru
     if (vrusSelected.length > 1) {
+      // show the second vru entry in legend
       let vru2 = document.getElementById("vru2_entry");
       vru2.style.display = "flex";
+
+      // add the correct vru text for each of the vru2 labels
       let vru2_fields = document.getElementsByClassName("vru2_label");
       for (let field of vru2_fields) {
         field.innerText = `${vruLabels[vrusSelected[1] - 1]}`;
       }
+
+      // fill in the value for the front dist for vru2
       let vru2_front = document.getElementById("vru2_front_dist");
       vru2_front.innerText = `${closest_forward_nvps[1]} ft`;
+
+      // fill in the value for the total number of vru2s in the blindzone
       let vru2_total_num = document.getElementById("vru2_total_num");
       vru2_total_num.innerText = `${num_vrus_in_vru_area[1]}`;
+
+      // fill in the plural vru2 text in the correct spots
       let vru2_label_plural = document.getElementById("vru2_label_plural");
       vru2_label_plural.innerText = `${vruLabelsPlural[vrusSelected[1] - 1]}`;
+
+      // show the text for vru2
       let vru2String = document.getElementById("graph_str2");
       vru2String.style.display = "inline";
+
+      // fill the vru2 label in the legend
       let vru2Text = document.getElementById("vru2_entry_text");
       vru2Text.innerText = `${vruLabels[vrusSelected[1] - 1]} blindzone`;
+
+      // set the color for vru2 and adjust the color for vru1
       let vru2Color = document.getElementById("vru2_entry_color");
       vru2Color.style.backgroundColor = colors[0];
       vru2Color.style.border = borders[0];
